c79ce8cbb197bd9b96d0dc1e27a9a29ef132da5c
Rayeen-Rahman <rayenrahman1@gmail.com>
2026-09-17 21:21:19 +0600

    Fix Windows DNS SRV resolution for MongoDB Atlas connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+// Fix for Node.js querySrv ECONNREFUSED on Windows
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
   try {
